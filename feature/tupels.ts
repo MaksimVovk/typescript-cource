3c6d13d98699f88db67d833d39e6d4414dfbecca
Max Vovk <maksemvovk@gmail.com>
2023-03-20 23:37:27 +0200

    typescript: interfaces

--- feature/tupels.ts
+++ feature/tupels.ts
@@ -9,3 +9,10 @@ type Drink = [string, boolean, number]
 const pepsi: Drink = ['brown', true, 40]
 const sprite: Drink = ['clear', true, 40]
 const tea: Drink = ['brown', false, 0]
+
+const carSpec: [number, number] = [400, 3566]
+
+const carStats = {
+  hoursepower: 400,
+  weight: 3566,
+}
